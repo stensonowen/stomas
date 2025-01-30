d6c04e3af74a75645a27eb75b11200c023b209a5
qj <qjkx@pm.me>
2025-01-30 11:16:07 -0500

    navbar.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,68 +1,12 @@
 <script lang="ts">
-    import logo from '../assets/sam1_.png'
-    import '@fortawesome/fontawesome-free/css/all.min.css'
+    import Navbar from './navbar.svelte'
 </script>
 
-<div class="flex flex-col items-center justify-center mt-32" >
-    <div class="flex flex-col">
+<Navbar />
 
-        <!-- Navbar -->
-        <nav class="flex justify-around py-4 bg-black/80
-            pt-2 pb-2
-            backdrop-blur-md w-full
-            fixed top-0 left-0 right-0 z-10">
+<div class="">
 
-            <!-- Logo Container -->
-            <div class="flex items-center">
-                <!-- Logo -->
-                <a class="cursor-pointer">
-                    <!--
-                        <span class="flex text-gray-300 hover:text-blue-500 cursor-pointer transition-colors duration-300"
-                        > Sam Thomas </span> -->
-                        <img class="h-10 object-cover"
-                            src={logo} >
-                    </a>
-                </div>
+    <h1>Welcome to SvelteKit</h1>
+    <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-                <!-- Links Section -->
-                <div class="items-center hidden space-x-8 lg:flex h20">
-                    <a class="flex text-gray-300 cursor-pointer text-2xl">
-                        Sam Thomas
-                    </a>
-
-                    <!--
-                    <a class="flex text-gray-300 hover:text-blue-500
-                        cursor-pointer transition-colors duration-300">
-                        Themes
-                    </a>
-                    <a class="flex text-gray-300 hover:text-blue-500
-                        cursor-pointer transition-colors duration-300">
-                        Developers
-                    </a>-->
-
-                </div>
-
-                <!-- Icon Menu Section -->
-                <div class="flex items-center space-x-5">
-                    <a class="flex text-gray-300 hover:text-blue-500 cursor-pointer transition-colors duration-200"
-                        href="bluesky.app" >
-                        <i class="fa-brands fa-bluesky"></i>
-                    </a>
-
-                    <a class="flex text-gray-300 hover:text-blue-500 cursor-pointer transition-colors duration-200"
-                        href="x.com/xkcd" >
-                        <i class="fa-brands fa-x-twitter"></i>
-                    </a>
-
-                    <a class="flex text-gray-300 hover:text-blue-500 cursor-pointer transition-colors duration-200"
-                        href="mailto@todo" >
-                        <i class="fa-regular fa-envelope"></i>
-                    </a>
-                </div>
-            </nav>
-        </div>
-
-    </div>
-
-        <h1>Welcome to SvelteKit</h1>
-        <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+</div>
---
+++ src/routes/navbar.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+    import logo from '../assets/sam1_.png'
+    import '@fortawesome/fontawesome-free/css/all.min.css'
+</script>
+
+<div class="flex flex-col mt-32">
+
+    <!-- Navbar -->
+    <nav class="flex justify-around py-4 bg-black/80
+        pt-2 pb-2
+        backdrop-blur-md w-full
+        fixed top-0 left-0 right-0 z-10">
+
+        <!-- Logo Container -->
+        <div class="flex items-center">
+            <!-- Logo -->
+            <a class="cursor-pointer">
+                <img class="h-10 object-cover"
+                    src={logo} alt="sam thomas headshot">
+                </a>
+            </div>
+
+            <!-- Links Section -->
+            <div class="items-center hidden space-x-8 lg:flex h20">
+                <a class="flex text-gray-300 cursor-pointer text-2xl">
+                    Sam Thomas
+                </a>
+
+                <!--
+                <a class="flex text-gray-300 hover:text-blue-500
+                    cursor-pointer transition-colors duration-300">
+                    Themes
+                </a>
+                <a class="flex text-gray-300 hover:text-blue-500
+                    cursor-pointer transition-colors duration-300">
+                    Developers
+                </a>-->
+
+            </div>
+
+            <!-- Icon Menu Section -->
+            <div class="flex items-center space-x-5">
+                <a class="flex text-gray-300 hover:text-blue-500 cursor-pointer transition-colors duration-200"
+                    href="bluesky.app" >
+                    <i class="fa-brands fa-bluesky"></i>
+                </a>
+
+                <a class="flex text-gray-300 hover:text-blue-500 cursor-pointer transition-colors duration-200"
+                    href="x.com/xkcd" >
+                    <i class="fa-brands fa-x-twitter"></i>
+                </a>
+
+                <a class="flex text-gray-300 hover:text-blue-500 cursor-pointer transition-colors duration-200"
+                    href="mailto@todo" >
+                    <i class="fa-regular fa-envelope"></i>
+                </a>
+            </div>
+        </nav>
+    </div>
+
