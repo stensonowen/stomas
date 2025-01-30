5a953980f0846566176db405d73757aa6a3ca03c
qj <qjkx@pm.me>
2025-01-30 16:07:34 -0500

    reusable .section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,27 @@
 <script lang="ts">
-    import Navbar from './navbar.svelte'
-    import bent from '../assets/bent1.png'
+    import Navbar from '$lib/navbar.svelte'
+    import bent from '../assets/bent2.png'
     import Card from '../card.svelte'
-    import Section from '../section.svelte'
 </script>
 
-<Navbar />
+<style>
+.section {
+    color: black;
+    border-radius: 15px;
+    overflow-y: hidden;
+    background-color: lightgray;
+    padding: 2rem;
+    margin-bottom: 1rem;
+    scroll-margin-top: 5rem;
+}
+.section h2 {
+    font-size: 2.25rem;
+    font-weight: bold;
+}
+</style>
 
+
+<Navbar />
 
 <div class="container mx-auto flex flex-col lg:flex-row">
 
@@ -14,9 +29,9 @@
     <div class="w-full lg:w-auto p-5 bg-white border rounded-xl border-gray-200 lg:fixed ">
       <h2 class="text-xl text-gray-700 font-bold mb-4">Contents</h2>
       <ul class="space-y-2 px-5">
-        <li><a href="#section1" class="text-gray-500 hover:font-bold">About</a></li>
-        <li><a href="#section3" class="text-gray-500 hover:font-bold">Publications</a></li>
-        <li><a href="#section2" class="text-gray-500 hover:font-bold">Courses</a></li>
+        <li><a href="#about"        class="text-gray-500 hover:font-bold">About</a></li>
+        <li><a href="#publications" class="text-gray-500 hover:font-bold">Publications</a></li>
+        <li><a href="#courses"      class="text-gray-500 hover:font-bold">Courses</a></li>
         <li><a href="#section4" class="text-gray-500 hover:font-bold">Papers</a></li>
         <li><a href="#section5" class="text-gray-500 hover:font-bold">Links</a></li>
       </ul>
@@ -25,12 +40,24 @@
     <!-- Content -->
     <div class="w-full lg:w-auto lg:ml-[15%] ">
 
-      <Section id="about" name="About">
-          <p> lorem ipsum dolor sit amet </p>
-      </Section>
+        <!-- ABOUT -->
+      <div id="about" class="section">
+          <img class="w-1/5 float-left mr-4 mb-4 rounded-lg" src={bent} alt="a man crouches" />
+          <h2> About Sam Thomas </h2>
+          <p class="text-xl py-1">
+            PhD student and Graduate TA at the University of Arizona. 
+          </p> <p class="text-xl py-1">
+            Research interests include legal epistemology, linguistics, critical theory, and European history.
+          </p>
+      </div>
 
-      <Section id="publications" name="Publications">
-      </Section>
+      <!-- PUBLICATIONS -->
+      <div id="publications" class="section">
+          <h2> Publications </h2>
+          <p class="text-xl text-gray-700">
+              helo
+          </p>
+      </div>
 
       <div id="section1" class="mb-8">
 
@@ -39,11 +66,6 @@
         <h2 class="text-4xl font-bold mb-4"> About </h2>
 
         <p class="text-xl text-gray-700">
-            PhD student and Graduate TA at the University of Arizona. 
-            <br>
-            Research interests include legal epistemology, linguistics, critical theory, and European history.
-            <br>
-            lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet
         </p>
       </div>
       <div id="section2" class="mb-8">
--- src/section.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-    let pr = $props();
-</script>
-
-<div id={pr.id} class="rounded-xl p-6 m-6 bg-gray-100" >
-    <h2 class="text-4xl font-bold mb-4"> {pr.name } </h2>
-</div>
