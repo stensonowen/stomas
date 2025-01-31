25ab9617da45c503fc22ccf70a7a92b303c5a5c9
qj <qjkx@pm.me>
2025-01-30 22:28:11 -0500

    navbar links

--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -8,24 +8,21 @@
     <!-- Navbar -->
     <nav class="flex justify-around py-4 bg-black/80 py-2 backdrop-blur-md w-full fixed top-0 left-0 right-0 z-10">
 
-        <!-- Logo Container -->
         <div class="flex items-center">
-            <!-- Logo -->
+            <!-- Left -->
             <a class="cursor-pointer">
-                <img class="h-10 object-cover"
-                    src={logo} alt="sam thomas headshot">
+                <img class="h-10 object-cover" href="/" src={logo} alt="sam thomas headshot">
                 </a>
             </div>
 
-            <!-- Links Section -->
+            <!-- Middle -->
             <div class="items-center p-2">
-                <a class="text-gray-200 cursor-pointer text-2xl">
+                <a class="text-gray-200 cursor-pointer text-2xl" href="/">
                     Sam Thomas
                 </a>
-
             </div>
 
-            <!-- Icon Menu Section -->
+            <!-- Right -->
             <div class="flex items-center space-x-5 text-gray-300 ">
                 <a class="flex cursor-pointer hover:text-blue-500 transition-colors duration-200" href="bluesky.app" >
                     <i class="fa-brands fa-bluesky"></i>
@@ -39,6 +36,7 @@
                     <i class="fa-regular fa-envelope"></i>
                 </a>
 
+                <!-- Light/Dark -->
                 <span class="pl-5">
                     <a class="hover:text-blue-500 cursor-pointer transition-colors duration-200 " >
                         <i class="fa-solid fa-sun"></i>
