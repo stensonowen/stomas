df537ed9189d8fc1df123a7c470ae1ef1278e40e
samueljthomasphilosophy <samthethomas@gmail.com>
2025-02-20 21:56:15 -0700

    Update +page.svelte

--- src/routes/Research/+page.svelte
+++ src/routes/Research/+page.svelte
@@ -102,7 +102,7 @@
             <h2> Academic Publications </h2>
             <Card
                 publisher="MA Thesis"
-                title="De Re <P Italic>Beliefs and Evidence in Legal Cases</P>"
+                title="De Re Beliefs and Evidence in Legal Cases"
                 link="https://philpapers.org/rec/THODRB"
                 date="2021"
                 text="For the past half-century, both jurisprudence and epistemology have been haunted by questions about why individual evidence (i.e., evidence which picks out a specific individual) can sufficiently justify a guilty or liable verdict while bare statistical evidence (i.e., statistical evidence which does not pick out a specific individual) does not sufficiently justify such a verdict. This thesis examines three popular justifications for such a disparity in verdicts – Judith Jarvis Thomson’s causal account, Enoch et al.’s sensitivity account, and Sarah Moss’ knowledge-first account, before critiquing each in turn. After such an analysis, the thesis then defends the claim that legal verdicts require the factfinder (e.g., the judge or jury) to have a justified de re belief (i.e., a belief about a specific object – namely the defendant), and that this doxastic requirement justifies the disparity in rulings, as it is epistemically insufficient to justify a de re belief based on bare statistical evidence alone. A brief account of how these beliefs are formed and spread is also given. After making such a distinction, the thesis then formalizes the burdens of proof of the preponderance of the evidence and beyond a reasonable doubt using the de re/de dicto distinction. Finally, the thesis pre-empts possible objections, namely by providing an account of DNA evidence as individual evidence and giving an account of how false convictions can occur on the de re view of legal proof."
@@ -114,16 +114,16 @@
             <h2> Public Philosophy </h2>
             Work of mine which is published outside the ivory tower.
             <Card
-                publisher="<P italic>Margin Notes</P>"
+                publisher="Margin Notes"
                 title="On the Falsity of Prevailing Ideas: The Concept of Ideology in Early Critical Theory"
                 link="https://ctwgwebsite.github.io/projects/1_project/"
                 date="Fall 2024"
-                text="Writers have spilled rivers of ink over the term “ideology.” This steady flow has become a deluge of mediocre tomes from uninquisitive minds..."
+                text="<P italic>Margin Notes</P> is the journal of the Critical Theory Working Group, of which I am a member. For more information, please see the CTWG tab."
                 pdf="https://ctwgwebsite.github.io/assets/pdf/journal/Margin_notes_1_final_full.pdf"
                 creativecommons="fa-creative-commons-by"
             />
             <Card
-                publisher="<P Italic>Cosmonaut Mag</P>"
+                publisher="Cosmonaut Mag"
                 title="The Machiavellian State, Fascism, and the Tribune of the Proletariat"
                 link="https://cosmonautmag.com/2022/04/the-machiavellian-state-fascism-and-the-tribune-of-the-proletariat/"
                 date="29 April 2022"
@@ -131,7 +131,7 @@
                 audio=true
             />
             <Card
-                publisher="<P Italic>Diginativ</P>"
+                publisher="Diginativ"
                 title="Spinoza with a Beard: Spinoza’s Influence on Marxism"
                 link="https://diginativ.com/spinoza-with-a-beard-spinozas-influence-on-marxism/"
                 date="5 May 2018 (the 200th birthday of Herr Marx)"
