5fc6cefb31ba53df67ebbaf0752610a7cde87067
samueljthomasphilosophy <samthethomas@gmail.com>
2025-02-20 22:07:29 -0700

    Create +page.svelte

---
+++ src/routes/ctwg/+page.svelte
@@ -0,0 +1,112 @@
+<script lang="ts">
+    import Navbar from '$lib/navbar.svelte'
+    import Card from '$lib/card.svelte'
+    
+    let contentsElems = {};
+
+    function initializeScrollObserver() {
+        document.querySelectorAll('#table-of-contentsElems a').forEach(elem => {
+            contentsElems[elem.hash] = elem;
+        });
+
+        const options = {
+            threshold: [0, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1],
+        };
+        let scrollObserver = new IntersectionObserver(onIntersection, options);
+        document.querySelectorAll('.section').forEach(e => scrollObserver.observe(e));
+    }
+    function onLoad() {
+        initializeScrollObserver();
+    }
+    function onIntersection(entries, observer) {
+        entries.forEach(e => {
+            if (e.isIntersecting) {
+                let link = contentsElems['#' + e.target.id];
+                if (! link) {
+                    console.warn(`label not found ${e.target.id}`);
+                    return
+                }
+                if (e.intersectionRatio > 0.4) {
+                    link.classList.add('selected');
+                } else {
+                    link.classList.remove('selected');
+                }
+            }
+        });
+    }
+
+</script>
+
+
+<style>
+    .section {
+        border-radius: 15px;
+        overflow-y: hidden;
+        background-color: lightgray;
+        padding: 1rem 2rem;
+        margin-bottom: 2rem;
+        scroll-margin-top: 5rem;
+    }
+    :global(body.dark) .section {
+        background-color: #0e1724;
+    }
+    .section h2 {
+        font-size: 2.25rem;
+        font-weight: bold;
+        margin-bottom: 1rem;
+    }
+    .section:target h2 {
+        text-decoration: underline;
+    }
+    .selected {
+        font-weight: bold;
+    }
+
+    :global(body) {
+        background-color: #EEEEEE;
+        transition: background-color 0.3s
+    }
+    :global(body.dark) {
+        background-color: #242529;
+    }
+</style>
+
+
+<Navbar />
+
+<div class="container mx-auto flex flex-col lg:flex-row">
+
+
+    <!-- Table of Contents -->
+    <div id="table-of-contentsElems" class="w-full lg:w-auto p-5 my-5 border rounded-xl border-gray-600 lg:fixed
+        bg-white dark:bg-gray-800
+        text-gray-500 dark:text-gray-300
+        ">
+        <h2 class="text-xl font-bold mb-4
+            text-gray-700 dark:text-gray-400"
+        >Contents</h2>
+        <ul class="space-y-2 px-5">
+            <li><a href="#ctwg" class="hover:font-bold">The Critical Theory Working Group</a></li>
+        </ul>
+    </div>
+
+    <!-- Content -->
+    <div use:onLoad id="content-area" class="w-full lg:w-auto lg:ml-[15%]
+        text-gray-600 dark:text-gray-300">
+        <!-- ABOUT -->
+        <div id="about" class="section">
+            <h2> The Critical Theory Working Group </h2>
+            <p class="text-xl pb-1 dark:text-gray-200">
+            The Critical Theory Working Group (CTWG) is a collective of researchers decicated to providing resources & open reading groups for collective study of early Critical Theory. We also have recently published a journal titled
+                <a class="text-italic"
+                >Margin Notes</a>. 
+            For more information, please visit the CTWG
+                </p> <p class="text-xl py-1">
+                <a class="text-blue-700 hover:underline" href="https://ctwgwebsite.github.io"
+                >website</a>. 
+                </p> <p class="text-xl py-1">
+            </p>
+        </div>
+    </div>
+</div>
+
