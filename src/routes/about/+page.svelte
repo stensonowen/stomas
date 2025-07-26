464b56e928cc58716594c349c782510905e7c241
qj <qjkx@pm.me>
2025-07-26 13:59:47 -0400

    checkout +page from main

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
     import Navbar from '$lib/navbar.svelte'
     import Card from '$lib/card.svelte'
-    // import bubbles from '../../assets/bubbles.jpeg'
-    import bubbles from '$lib/../assets/bubbles.jpeg'
     
     let contentsElems = {};
 
@@ -98,7 +96,6 @@
         <!-- ABOUT -->
         <div id="about" class="section">
             <h2> About Me </h2>
-            <img src={bubbles} alt="gettin lit off de bubbles" />
             <p class="text-xl pb-1 dark:text-gray-200">
                 I go by Sam around friends and Mr. Thomas around state apparatchiks, authority figures, and the like.
             
@@ -116,3 +113,4 @@
         </div>
     </div>
 </div>
+
